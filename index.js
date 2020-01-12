fcb6ff9cebaa58d3771e9276aa98e1c227762a23
Thomas Nguyen <ntthang123@gmail.com>
2020-01-11 22:11:06 -0800

    Add title

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const height = +svg.attr('height');
 const render = data => {
     const xValue = d => d.population;
     const yValue = d => d.country;
-    const margin = {top: 20, right: 60, bottom: 40, left: 100};
+    const margin = {top: 50, right: 60, bottom: 60, left: 140};
     const innerWidth = width - margin.left - margin.right;
     const innerHeight = height - margin.top -margin.bottom;
 
@@ -19,23 +19,49 @@ const render = data => {
         .range([0,innerHeight])
         .padding(0.1);
 
-    const xAxis = d3.axisBottom(xScale);    
+    const xAxisTickFormat = number => 
+        d3.format('.3s')(number)
+        .replace('G','B');
+
+    const xAxis = d3.axisBottom(xScale)
+        .tickFormat(xAxisTickFormat)
+        .tickSize(-(innerHeight));
+
     const yAxis = d3.axisLeft(yScale);
-    
 
     const g = svg.append('g')
         .attr('transform', `translate(${margin.left}, ${margin.top})`);
 
-    g.append('g').call(yAxis);
-    g.append('g').call(xAxis)
-        .attr('transform', `translate(0, ${innerHeight})`);
+    g.append('g')
+        .call(yAxis)
+        .selectAll('.domain, .tick line')
+            .remove();
+
+    xAxisG =   g.append('g')
+        .call(xAxis)
+        .attr('transform', `translate(0, ${innerHeight})`)      
+
+    
+    xAxisG.select('.domain').remove();
+
+    xAxisG.append('text')
+        .attr('class', 'axis-label')
+        .attr('y', 50)
+        .attr('x',innerWidth/2)
+        .attr('fill', 'black')
+        .text('Population')
 
     g.selectAll('rect').data(data)
         .enter().append('rect')
             .attr('y', d => yScale(yValue(d)))
             .attr('width', d => xScale(xValue(d)))
             .attr('height', yScale.bandwidth())
-            .attr('transform', `translate(2,0)`)
+            .attr('transform', `translate(2,0)`);
+    
+    g.append('text')
+        .attr('class', 'title')
+        .attr('y',-10)
+        .text('Top 10 most populated countries');
 };
 
 d3.csv('countrydata.csv').then(data => {
